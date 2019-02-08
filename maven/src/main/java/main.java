9530503445a761bd0ad13cf18a0958d2c3a8212c
BOG-A404-E-013 <BOG-A404-E-013@RGH-LS26-13.UMD.LOCAL.EDU>
2019-02-07 19:29:46 -0500

    inclui una libreria jodatime

---
+++ maven/src/main/java/main.java
@@ -0,0 +1,25 @@
+
+import java.time.LocalTime;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author BOG-A404-E-013
+ */
+public class main {
+    
+    
+    public static void main(String[] args) {
+        
+        LocalTime tiempo = new LocalTime();         
+               System.out.print("El tiempo actual es "+ tiempo);
+
+        
+    }
+    
+}
